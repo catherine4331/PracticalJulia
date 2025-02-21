33c434c80e908ff416a105f8276fb02d7e726779
Catherine Reid <catherine.reid@neo4j.com>
2025-02-21 16:56:16 +0000

    Fun with types

--- Types/src/Types.jl
+++ Types/src/Types.jl
@@ -43,20 +43,148 @@ nodeshape=:rect,
 a::Int16 = 17
 typeof(a)
 
-function weather_report(raining::Bool)
+function weather_report(raining::Bool, city::String)
     if raining
         n = ""
     else
         n = "not "
     end
-    gf = "London"
-    println("It is $(n)raining in $gf today.")
+    println("It is $(n)raining in $city today.")
 end
 
 function weather_report(raining) 
     println("Please tell us if it's raining with \"true\" or 
 \"false\".") 
     return 
+end
+
+methods(weather_report)
+
+# We can extend build in functions with new methods
+import Base.+
+function +(a::Number, b::String)
+    if Meta.parse(b) isa Number
+        return a + Meta.parse(b)
+    else
+        return a
+    end
+end
+
+1 + "1"
+
+
+#Unions
+17 isa Union{Number, String}
+Real <: Union{Number, String}
+
+# We can declare our own types
+abstract type MyNumber <: Number end
+struct EarthLocation
+    latitude::Float64
+    longitude::Float64
+    timezone::String
+end
+EarthLocation(a, b) = EarthLocation(a, b, "Unknown")
+
+NYC = EarthLocation(40.7128, -74.0060, "EDT")
+typeof(NYC)
+NYC.timezone
+
+abstract type Circle end
+
+struct FloatingCircle <: Circle
+    r::Real
+end
+
+struct PositionedCircle <: Circle
+    x::Real
+    y::Real
+    r::Real
+end
+
+function circle_area(c::Circle)
+    return π * c.r^2
+end
+
+function is_inside(c1::PositionedCircle, c2::PositionedCircle)
+    d = sqrt((c2.x - c1.x)^2 + (c2.y - c1.y)^2)
+    return d + c2.r < c1.r
+end
+
+# This is a nice macro for creating constructors
+@kwdef struct Ellipse
+    axis1::Real = 1
+    axis2::Real = 1
+end
+
+oval = Ellipse(axis2=2.6)
+
+# For good performance, we generally want to ensure types are stable
+# This function is problematic - We get a float back if b is not 0, but
+# an integer if b is 0
+function safe_divide(a, b)
+    if b == 0
+        return 0
+    else
+        return a/b
+    end
+end
+
+# This is so important that Julia has a macro to help us with it
+@code_warntype safe_divide(1, 2)
+
+# Let's fix it and see what happens
+function safe_divide_better(a, b)
+    if b == 0
+        return 0.0
+    else
+        return a/b
+    end
+end
+@code_warntype safe_divide_better(1, 2)
+
+# Another type stability gotcha is changing the type of a variable
+function leibn(N)
+    s = 0
+    for n in 1:N
+        s += (-1)^(n+1) * 1/(2n-1)
+    end
+    return 4.0s
+end
+
+# Once again code_warntype can help us
+@code_warntype leibn(100)
+        
+# We can create our own type aliases
+const F64 = Float64
+
+# We can also create parametric types
+@kwdef struct CEllipse{T<:Number}
+    axis1::T
+    axis2::T
+end
+
+# Now, because we are taking any number, it could be complex. So we need
+# specialisation in some methods to handle this
+function eccentricity(e::CEllipse{<:Real})
+    a = max(e.axis1, e.axis2)
+    b = min(e.axis1, e.axis2)
+    return sqrt(a^2 - b^2)/a
+end
+
+function eccentricity(e::CEllipse{<:Complex})
+    a = max(abs(e.axis1), abs(e.axis2))
+    b = min(abs(e.axis1), abs(e.axis2))
+    return sqrt(a^2 - b^2)/a
+end
+
+function orientation(e::CEllipse{<:Complex})
+    if abs(e.axis1) > abs(e.axis2)
+        a = e.axis1
+    else
+        a = e.axis2
+    end
+    return angle(a)
 end
 
 end # module Types
